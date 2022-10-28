a17c6d43e91b817ddc0b941f74cfb0034dbce4b1
Zaid El Jattari <zaid.26102390@gmail.com>
2022-10-28 13:53:16 +0200

    “push”

---
+++ index.js
@@ -0,0 +1,46 @@
+import fs from "fs"; //import nodejs core = (ingebakken) library
+
+const folderToRead = process.argv[2]; //grab third array argument
+
+if (!fs.existsSync(folderToRead)) {
+    //check if folder does not exist
+  console.log("Folder could not be found !!!");
+} else {
+    //folder exists...
+  const folderContents = fs.readdirSync(folderToRead);
+  let allContent = "";
+  for (let i = 0; i < folderContents.length; i++) {
+    allContent += fs.readFileSync(folderToRead + "/" + folderContents[i]);
+  }
+  allContent = allContent
+    .replace(/[^a-z]/gi, " ") // ^ = alles behalve 
+    .replace(/font|color/gi, " ")
+    .replace(/ +/g, " ")
+    .toLowerCase()
+    .split(" ")
+    .filter(function (word) {
+      return word.length >= 4;
+    });
+
+  const wordsWithOccurrence = allContent.reduce(function (obj, word) {
+    obj[word] = obj[word] + 1 || 1;
+    return obj;
+  }, {});
+
+  const uniqueWords = Object.keys(wordsWithOccurrence);
+
+  uniqueWords.sort(function (wordA, wordB) {
+    return wordsWithOccurrence[wordB] - wordsWithOccurrence[wordA];
+  });
+
+  console.log(uniqueWords);
+}
+
+
+
+
+
+
+
+
+
